e336dc45fccd5ca0b539c142d1554f4aa7eba4c7
Roy <roytseng.tw@gmail.com>
2018-03-15 15:43:06 +0800

    Fix resnet backbone for res50 and res152

--- lib/model/mask_rcnn/resnet.py
+++ lib/model/mask_rcnn/resnet.py
@@ -7,12 +7,13 @@ import torch.nn as nn
 
 from model.utils.config import cfg
 from model.mask_rcnn.mask_rcnn import _maskRCNN
-from model.faster_rcnn.resnet import resnet101
+from model.faster_rcnn.resnet import resnet50, resnet101, resnet152  # NOQA
 
 
 class resnet(_maskRCNN):
   def __init__(self, classes, num_layers=101, pretrained=False, class_agnostic=False):
-    self.model_path = 'data/pretrained_model/resnet101_caffe.pth'
+    self.num_layers = num_layers
+    self.model_path = 'data/pretrained_model/resnet%d_caffe.pth' % num_layers
     self.dout_base_model = 1024
     self.pretrained = pretrained
     self.class_agnostic = class_agnostic
@@ -20,12 +21,12 @@ class resnet(_maskRCNN):
     super().__init__(classes, class_agnostic)
 
   def _init_modules(self):
-    resnet = resnet101()
+    resnet = eval('resnet%d()' % self.num_layers)
 
     if self.pretrained:
       print("Loading pretrained weights from %s" % (self.model_path))
       state_dict = torch.load(self.model_path)
-      resnet.load_state_dict({k:v for k,v in state_dict.items() if k in resnet.state_dict()})
+      resnet.load_state_dict({k: v for k, v in state_dict.items() if k in resnet.state_dict()})
 
     # Build resnet.
     self.RCNN_base = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu,
@@ -40,21 +41,21 @@ class resnet(_maskRCNN):
       self.RCNN_bbox_pred = nn.Linear(2048, 4 * self.n_classes)
 
     # Fix blocks
-    for p in self.RCNN_base[0].parameters(): p.requires_grad = False
-    for p in self.RCNN_base[1].parameters(): p.requires_grad = False
+    for p in self.RCNN_base[0].parameters(): p.requires_grad = False  # NOQA
+    for p in self.RCNN_base[1].parameters(): p.requires_grad = False  # NOQA
 
     assert (0 <= cfg.RESNET.FIXED_BLOCKS < 4)
     if cfg.RESNET.FIXED_BLOCKS >= 3:
-      for p in self.RCNN_base[6].parameters(): p.requires_grad = False
+      for p in self.RCNN_base[6].parameters(): p.requires_grad = False  # NOQA
     if cfg.RESNET.FIXED_BLOCKS >= 2:
-      for p in self.RCNN_base[5].parameters(): p.requires_grad = False
+      for p in self.RCNN_base[5].parameters(): p.requires_grad = False  # NOQA
     if cfg.RESNET.FIXED_BLOCKS >= 1:
-      for p in self.RCNN_base[4].parameters(): p.requires_grad = False
+      for p in self.RCNN_base[4].parameters(): p.requires_grad = False  # NOQA
 
     def set_bn_fix(m):
       classname = m.__class__.__name__
       if classname.find('BatchNorm') != -1:
-        for p in m.parameters(): p.requires_grad = False
+        for p in m.parameters(): p.requires_grad = False  # NOQA
 
     self.RCNN_base.apply(set_bn_fix)
     self.RCNN_top.apply(set_bn_fix)
