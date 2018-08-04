cb237c0d99c22946417dc57f6b8a1605366891e1
vfdev-5 <vfdev.5@gmail.com>
2018-08-04 13:47:03 +0200

    Fix compiling errors for pytorch 0.4.1

--- lib/model/nms/src/nms_cuda.c
+++ lib/model/nms/src/nms_cuda.c
@@ -8,12 +8,12 @@ extern THCState *state;
 int nms_cuda(THCudaIntTensor *keep_out, THCudaTensor *boxes_host,
 		     THCudaIntTensor *num_out, float nms_overlap_thresh) {
 
-	nms_cuda_compute(THCudaIntTensor_data(state, keep_out), 
-		         THCudaIntTensor_data(state, num_out), 
-      	                 THCudaTensor_data(state, boxes_host), 
-		         boxes_host->size[0], 
-		         boxes_host->size[1],
-		         nms_overlap_thresh);
+    nms_cuda_compute(THCudaIntTensor_data(state, keep_out),
+                     THCudaIntTensor_data(state, num_out),
+                     THCudaTensor_data(state, boxes_host),
+                     THCudaTensor_size(state, boxes_host, 0),
+                     THCudaTensor_size(state, boxes_host, 1),
+                     nms_overlap_thresh);
 
 	return 1;
 }
